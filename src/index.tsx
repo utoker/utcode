18afe847a2513b92cd98f47427703599e534f743
Umut Toker <utoker@gmail.com>
2022-01-22 18:05:58 -0500

    add exbuild-wasm

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,38 @@
-import ReactDOM from "react-dom";
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
 
 const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const ref = useRef<any>();
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+    console.log(ref.current);
+  };
+  const onClick = () => {
+    console.log(input);
+  };
   return (
-    <div>
-      <h1>UTCODE</h1>
-    </div>
+    <>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
