47714f3c6db8f2b2371858d8f53d3ad393db41d4
Umut Toker <utoker@gmail.com>
2022-01-25 17:05:28 -0500

    add esbuld.wasm

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,35 @@
 import * as esbuild from 'esbuild-wasm';
-import { useEffect, useRef, useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
-  const ref = useRef<any>();
-
-  useEffect(() => {
-    startService();
-  }, []);
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm',
     });
-    console.log(ref.current);
   };
-  const onClick = () => {
-    console.log(input);
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+    setCode(result.code);
   };
+
   return (
-    <>
+    <div>
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
@@ -31,7 +38,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-    </>
+    </div>
   );
 };
 
