2b31bf6e95955d9318eef91ac9164c365e2b32db
Umut Toker <utoker@gmail.com>
2022-02-01 15:59:32 -0500

    refactor caching

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -16,7 +16,7 @@ export const fetchPlugin = (inputCode: string) => {
           contents: inputCode,
         };
       });
-      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
@@ -24,7 +24,8 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
-
+      });
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const escaped = data
@@ -46,14 +47,6 @@ export const fetchPlugin = (inputCode: string) => {
         return result;
       });
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-
-        if (cachedResult) {
-          return cachedResult;
-        }
-
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
