73084c1d6580f3799a661b9470379d5832834942
Umut Toker <utoker@gmail.com>
2022-02-04 18:07:32 -0500

    add prettier to monaco code editor

--- src/components/monaco-editor.tsx
+++ src/components/monaco-editor.tsx
@@ -1,7 +1,59 @@
-import MonacoEditor from '@monaco-editor/react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import { useRef } from 'react';
 
-const CodeEditor = () => {
-  return <MonacoEditor height="500px" />;
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const editorRef = useRef<any>();
+
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+  };
+  const onFormatClick = () => {
+    //get current value from
+    const unformatted = editorRef.current.getModel().getValue();
+    // format that value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+    // set the formatted value back in the editor
+    editorRef.current.setValue(formatted);
+  };
+
+  return (
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLine: false,
+          automaticLayout: true,
+        }}
+        theme="dark"
+        language="javascript"
+        height="500px"
+      />
+    </div>
+  );
 };
 
 export default CodeEditor;
