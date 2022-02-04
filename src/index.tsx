df1c86cc8a444459ebb4cd0de2b95ceea66113e9
Umut Toker <utoker@gmail.com>
2022-02-03 20:43:10 -0500

    add iframe

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
@@ -34,10 +35,29 @@ const App = () => {
       },
     });
 
-    // console.log(result);
-
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+<html>
+<head></head>
+<body>
+<div id="root"></div>
+<script>
+  window.addEventListener('message',(event)=>{
+    try {
+      eval(event.data);
+    } catch(err) { 
+    const root = document.querySelector('#root');
+    root.innerHTML = '<div style="color:red;"><h4>Runtime Error</h4>' + err +' </div>'
+    console.error(err)
+    }
+  },false);
+</script>
+</body>
+</html>
+`;
 
   return (
     <div>
@@ -49,6 +69,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe ref={iframe} srcDoc={html} sandbox="allow-scripts" />
     </div>
   );
 };
