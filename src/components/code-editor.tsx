53dc724dd1637d93b69bc5bc8a8a5e2628341a17
Umut Toker <utoker@gmail.com>
2022-02-08 16:32:05 -0500

    add monaco jsx highlighter

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,11 @@
-import MonacoEditor, { OnChange } from '@monaco-editor/react';
+import MonacoEditor, { OnChange, OnMount } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
+import './syntax.css';
 import './code-editor.css';
+import { parse } from '@babel/parser';
+import traverse from '@babel/traverse';
+import MonacoJSXHighlighter from 'monaco-jsx-highlighter';
 
 interface CodeEditorProps {
   value: string;
@@ -9,6 +13,17 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ value, onChange }) => {
+  const handleEditorDidMount: OnMount = (editor, monaco) => {
+    const monacoJSXHighlighter = new MonacoJSXHighlighter(
+      monaco, // references Range and other APIs
+      parse, // obtains an AST, internally passes to parse options: {...options, sourceType: "module",plugins: ["jsx"],errorRecovery: true}
+      traverse, // helps collecting the JSX expressions within the AST
+      editor // highlights the content of that editor via decorations
+    );
+
+    monacoJSXHighlighter.highlightOnDidChangeModelContent();
+  };
+
   const handleEditorChange: OnChange = (code) => {
     onChange(code || '');
   };
@@ -35,6 +50,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ value, onChange }) => {
         Format
       </button>
       <MonacoEditor
+        onMount={handleEditorDidMount}
         theme="vs-dark"
         height="100%"
         defaultLanguage="javascript"
