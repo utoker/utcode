189e6c22bdb854928465699d5fa5b2a7fef07747
Umut Toker <utoker@gmail.com>
2022-05-14 13:33:15 -0400

    add loading style to code cell

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css';
 import { useEffect } from 'react';
 import { useActions } from '../hooks/use-actions';
 import { Cell } from '../state';
@@ -37,7 +38,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} BundleStatus={bundle.err} />}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} BundleStatus={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
